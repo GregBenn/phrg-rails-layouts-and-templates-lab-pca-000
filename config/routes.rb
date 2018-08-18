0bd679aefcb087ae1ee626b4757cf1e74d572b16
Greg Benn <gbenn07@gmail.com>
2018-08-18 18:31:24 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'home', to: 'static#home'
-  get 'admin/home', to: 'store_admin#home'
-  get 'admin/orders', to: 'store_admin#orders'
-  get 'admin/invoice', to: 'store_admin#invoice'
+  get "home", to: "static#home"
+  get "admin/home", to: "store_admin#home"
+  get "admin/orders", to: "store_admin#orders"
+  get "admin/invoice", to: "store_admin#invoice"
 end
--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -1,4 +1,6 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.describe StaticController, type: :controller do
   include Capybara::DSL
